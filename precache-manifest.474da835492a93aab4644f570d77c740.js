1f0a008dd90a2894f8164616adb7e86455ce2800
Arnau Gómez Farell <agomez1f@gmail.com>
2020-08-04 10:41:15 +0200

    Updates

---
+++ precache-manifest.474da835492a93aab4644f570d77c740.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0898062f778181301bb643a7f75ff885",
+    "url": "/to-do-list-1/index.html"
+  },
+  {
+    "revision": "0937823f3706f505c572",
+    "url": "/to-do-list-1/static/js/2.4dec70f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list-1/static/js/2.4dec70f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97bc9d83f71be1e28467",
+    "url": "/to-do-list-1/static/js/main.667e4da6.chunk.js"
+  },
+  {
+    "revision": "c65d40199af044590c60",
+    "url": "/to-do-list-1/static/js/runtime-main.2376ef5a.js"
+  }
+]);
